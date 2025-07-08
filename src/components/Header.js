6ebc30106004e979bc713dc51d978d8053e5104a
hikmet <thatshiko@gmail.com>
2025-07-08 11:11:53 +0300

    style(navbar): navbar degisti

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,7 @@ import './Header.css';
 const Header = () => {
   return (
     <header className="header">
-      <div className="container header-content">
-        <div className="logo">
-          <NavLink to="/">React Template</NavLink>
-        </div>
-        <nav className="nav">
-          <ul className="nav-list">
-            <li className="nav-item">
-              <NavLink to="/" className={({isActive}) => isActive ? "active" : ""}>
-                Home
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink to="/about" className={({isActive}) => isActive ? "active" : ""}>
-                About
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
+        <div>Navbar</div>
     </header>
   );
 };
