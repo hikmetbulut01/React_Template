ca7489c39b1ec11c130951a8551c5b2bb953d681
hikmet <thatshiko@gmail.com>
2025-07-08 11:13:24 +0300

    style(footer): Footer degisti
    style(footer): Footer degisti

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,20 +2,11 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  const currentYear = new Date().getFullYear();
+  
   
   return (
     <footer className="footer">
-      <div className="container footer-content">
-        <div className="copyright">
-          &copy; {currentYear} React Template. All rights reserved.
-        </div>
-        <div className="footer-links">
-          <a href="/privacy" className="footer-link">Privacy</a>
-          <a href="/terms" className="footer-link">Terms</a>
-          <a href="/contact" className="footer-link">Contact</a>
-        </div>
-      </div>
+        <div>footer</div>
     </footer>
   );
 };
